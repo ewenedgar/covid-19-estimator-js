0f45821f0b2a6ede4f23e605f58ebf964f876dbf
ewen <ewenedgar@gmail.com>
2020-04-13 17:09:09 +0300

    challenge 3

--- src/estimator.js
+++ src/estimator.js
@@ -8,7 +8,7 @@ const covid19ImpactEstimator = () => {
       avgDailyIncomePopulation: 0.71
     },
     periodType: 'days',
-    timeToElapse: 58,
+    timeToElapse: 7,
     reportedCases: 674,
     population: 66622705,
     totalHospitalBeds: 1380614
