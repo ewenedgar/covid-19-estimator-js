33e7eb1686003d11cbc1082e10528c9c33e66c56
ewen <ewenedgar@gmail.com>
2020-04-13 17:55:13 +0300

    challenge 3

--- src/estimator.js
+++ src/estimator.js
@@ -1,19 +1,5 @@
 
-const covid19ImpactEstimator = () => {
-  const data = {
-    region: {
-      name: 'Africa',
-      avgAge: 19.7,
-      avgDailyIncomeInUSD: 5,
-      avgDailyIncomePopulation: 0.71
-    },
-    periodType: 'days',
-    timeToElapse: 7,
-    reportedCases: 674,
-    population: 66622705,
-    totalHospitalBeds: 1380614
-  };
-
+const covid19ImpactEstimator = (data) => {
   function days() {
     let numDays;
     if (data.periodType === 'weeks') {
