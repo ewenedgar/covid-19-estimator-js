27108119558e2bafcf202e5426e8dc61c16fe3a9
ewen <ewenedgar@gmail.com>
2020-04-14 05:21:07 +0300

    challenge3

--- src/estimator.js
+++ src/estimator.js
@@ -21,9 +21,7 @@ const covid19ImpactEstimator = (data) => {
     const hospitalBedsByRequestedTime = (Math.ceil((
       (35 / 100) * data.totalHospitalBeds)) - severeCasesByRequestedTime);
     const casesForICUByRequestedTime = (Math.floor((5 / 100) * infectionsByRequestedTime));
-    const casesForVentilatorsByRequestedTime = (Math.floor(
-      (0.02) * infectionsByRequestedTime
-    ));
+    const casesForVentilatorsByRequestedTime = (Math.floor((0.02) * infectionsByRequestedTime));
     const dollarsInFlight = parseFloat((infectionsByRequestedTime * 0.65 * (
       data.region.avgDailyIncomeInUSD) * (days(data.timeToElapse))).toFixed(2));
 
@@ -43,9 +41,7 @@ const covid19ImpactEstimator = (data) => {
     const hospitalBedsByRequestedTime = (Math.ceil((
       (35 / 100) * data.totalHospitalBeds)) - severeCasesByRequestedTime);
     const casesForICUByRequestedTime = (Math.floor((5 / 100) * infectionsByRequestedTime));
-    const casesForVentilatorsByRequestedTime = (Math.floor(
-      (0.02) * infectionsByRequestedTime
-    ));
+    const casesForVentilatorsByRequestedTime = (Math.floor((0.02) * infectionsByRequestedTime));
     const dollarsInFlight = parseFloat((infectionsByRequestedTime * 0.65 * (
       data.region.avgDailyIncomeInUSD) * (days(data.timeToElapse))).toFixed(2));
 
