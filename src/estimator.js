9a01e16c8ff4d0fe93d57d046b9a1f480536b0de
ewen <ewenedgar@gmail.com>
2020-04-14 13:02:34 +0300

    challenge3

--- src/estimator.js
+++ src/estimator.js
@@ -31,12 +31,8 @@ const covid19ImpactEstimator = (data) => {
     const severeCasesByRequestedTime = (Math.floor((15 / 100) * infectionsByRequestedTime));
     const hospitalBedsByRequestedTime = (Math.trunc((
       (35 / 100) * data.totalHospitalBeds) - severeCasesByRequestedTime));
-    const casesForICUByRequestedTime = (Math.floor(
-      (5 / 100) * infectionsByRequestedTime
-    ) - 1);
-    const casesForVentilatorsByRequestedTime = (Math.trunc(
-      (0.02) * infectionsByRequestedTime
-    ) - 1);
+    const casesForICUByRequestedTime = (Math.floor((5 / 100) * infectionsByRequestedTime));
+    const casesForVentilatorsByRequestedTime = (Math.trunc((0.02) * infectionsByRequestedTime));
     const dollarsInFlight = Math.trunc(((
       infectionsByRequestedTime * data.region.avgDailyIncomePopulation * (
         data.region.avgDailyIncomeInUSD)) / (Days(data.timeToElapse))).toFixed());
@@ -56,12 +52,10 @@ const covid19ImpactEstimator = (data) => {
     const severeCasesByRequestedTime = (Math.floor((15 / 100) * infectionsByRequestedTime));
     const hospitalBedsByRequestedTime = (Math.trunc((
       (35 / 100) * data.totalHospitalBeds) - severeCasesByRequestedTime));
-    const casesForICUByRequestedTime = Number(Math.floor(
-      (5 / 100) * infectionsByRequestedTime
-    ) - 1);
+    const casesForICUByRequestedTime = Number(Math.floor((5 / 100) * infectionsByRequestedTime));
     const casesForVentilatorsByRequestedTime = Number(Math.trunc(
       (0.02) * infectionsByRequestedTime
-    ) - 1);
+    ));
     const dollarsInFlight = Math.trunc((
       infectionsByRequestedTime * data.region.avgDailyIncomePopulation * (
         data.region.avgDailyIncomeInUSD)) / (Days(data.timeToElapse)));
